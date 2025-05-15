1636a6523c38b0285045bb57a2fe05a454cbb67d
shidorn <78392833+shidorn@users.noreply.github.com>
2025-05-15 13:16:45 +0800

    Update app.service.ts

--- final-todo-app-api/src/app.service.ts
+++ final-todo-app-api/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World Sheldon Cloud Build!';
+    return 'Hello World Sheldon Cloud Build we!';
   }
 }
